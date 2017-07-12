be7e4a18963e3623c5c81b0e29234c03971b8269
Lynn Bradshaw <readyready15728@gmail.com>
2017-07-12 01:20:41 -0400

    Revamping ratings.py, using PyQuery instead of lxml

--- ratings.py
+++ ratings.py
@@ -1,52 +1,60 @@
+import csv
+import re
 import sys
-import requests
-import socket
-from lxml import html
+from collections import namedtuple
+from pyquery import PyQuery as pq
+
+Record = namedtuple('Record', ['title', 'avg_rating', 'no_ratings'])
 
 games_with_ratings = []
-listing_uri = sys.argv[1]
-
-def resilient_get(uri, timeout=5):
-    while True:
-        try:
-            page = requests.get(uri, timeout=timeout)
-            return page
-        except:
-            pass
+# Starts as e.g. http://www.myabandonware.com/browse/genre/simulation-7/ then
+# iterates through all the numbered pages listed in that category in the
+# range [2, n] where n is the last page
+category_uri = sys.argv[1]
 
 while True:
-    listing_page = resilient_get(listing_uri)
-    listing_page.encoding = 'utf-8'
-    listing_page_tree = html.fromstring(listing_page.text)
-    listing_page_tree.make_links_absolute(listing_uri)
+    category_page = pq(category_uri)
+    category_page.make_links_absolute(base_url='http://myabandonware.com/')
 
-    for i in listing_page_tree.xpath('//div[@class="name"]/a'):
-        game_uri = list(i.iterlinks())[0][2]
-        game_page = resilient_get(game_uri)
-        game_page.encoding = 'utf-8'
-        game_page_tree = html.fromstring(game_page.text)
+    # Runs through the <a> tags in the .name divs which link to individual
+    # games in the category
+    for game_link in category_page('.name > a'):
+        game_link = pq(game_link)
 
-        title = i.text
-        platform = i.xpath('../following-sibling::div[@class="platyear"]/span[@class="ptf"]')[0].text
-        release_year = i.xpath('../following-sibling::div[@class="platyear"]/span[@class="year"]')[0].text
-        title = '%s (%s - %s)' % (title, platform, release_year)
+        title = game_link.text()
+        # There is a <div> after the <div> containing the link to the game
+        # page with this good stuff in it
+        platform_and_year = game_link.parent().next()
+        platform = platform_and_year.find('.ptf').text()
+        year = int(platform_and_year.find('.year').text())
+        # Will look something like: The Settlers II: Gold Edition (DOS - 1997)
+        title = '%s (%s - %d)' % (title, platform, year)
 
-        try:
-            rating = float(game_page_tree.xpath('//span[@itemprop="ratingValue"]/text()')[0])
-            votes = int(game_page_tree.xpath('//span[@itemprop="ratingCount"]/text()')[0])
-            games_with_ratings.append((title, rating, votes))
-        except IndexError:
-            games_with_ratings.append((title, 0, 0))
-            
+        game_uri = game_link.attr('href')
+        game_page = pq(game_uri)
+        # The <div> selected here will contain text looking something like:
+        # 4.55 / 5 - 119 votes
+        # 
+        # The question mark is there because some of the pages only say "vote"
+        # due to the number of votes
+        m = re.search('((?:\d|\.)+) / 5 - (\d+) votes?', game_page('.gameRated').text())
+        avg_rating = float(m.group(1))
+        no_ratings = int(m.group(2))
+
+        # The process can take a while so we'll want logging
         sys.stderr.write(title + '\n')
+        games_with_ratings.append(Record(title, avg_rating, no_ratings))
 
-    try:
-        listing_uri = list(listing_page_tree.xpath('//a[@class="current"]/following-sibling::a[position()=1]')[0].iterlinks())[0][2]
-    except IndexError:
+    # The current page is marked with the class "current". If we can find a
+    # successor that is an <a> tag, we move on to that. If not, we quit and 
+    # write out the csv.
+    category_uri = category_page('.current').next().attr('href')
+
+    if category_uri is None:
         break
 
-print ','.join(['Title', 'Average Rating', 'Number of Ratings'])
+writer = csv.writer(sys.stdout)
+writer.writerow(['Title', 'Average Rating', 'Number of Ratings'])
 
-for i in games_with_ratings:
-    # Escape double quotes
-    print ','.join(['"%s"' % i[0].replace('"', '""'), str(i[1]), str(i[2])])
+for record in games_with_ratings:
+    writer.writerow([record.title, '%.2f' % record.avg_rating, record.no_ratings])
