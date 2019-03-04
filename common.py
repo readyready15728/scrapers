2f1ff6873083cfd782e1684ebea7b8f126f1e47b
Lynn Bradshaw <readyready15728@gmail.com>
2019-03-04 00:41:12 -0500

    Spoofing request from browser and pausing between requests to avoid the ire of My Abanonware

--- common.py
+++ common.py
@@ -1,13 +1,15 @@
 import urllib
 import requests
+import time
 from bs4 import BeautifulSoup
 
 # Don't crap out trying to get data from the server; just keep trying
 def resilient_get(uri):
     while True:
         try:
-            return requests.get(uri, timeout=None)
+            return requests.get(uri, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0'}, timeout=None)
         except:
+            time.sleep(5)
             pass
 
 def beautify(request):
